b44c3ec3ce3f3b48bd4ab278ab0499cab4939949
Diego Henrique <diegohrp20@gmail.com>
2022-12-29 00:24:02 -0300

    Criando o metodo get para listagem de medicos.

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,13 +1,17 @@
 package med.voll.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 
@@ -22,5 +26,11 @@ public class MedicoController {
 	@Transactional
 	public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
 		repository.save(new Medico(dados));
+	}
+
+	@GetMapping
+	public List<DadosListagemMedico> listar() {
+		// Dessa forma converto uma lista de Medico para uma lista DadosListagemMedico
+		return repository.findAll().stream().map(DadosListagemMedico::new).toList();
 	}
 }
