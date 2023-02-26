c0d815831e252b278cb89ba5f1dc584502976ead
Diego Henrique <diegohrp20@gmail.com>
2023-02-25 21:18:31 -0300

    Criando a funcionalidade de agendamento de consultas, cirando através de migrations a tabela consultas, juntamente com seu respectivo controller e dtos(record) de agendamento e detalhamento as consultas, além de um gitignore, para ignorar o .vscode.

--- api/src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ api/src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -4,11 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
 import med.voll.api.domain.usuario.DadosAutenticacao;
 import med.voll.api.domain.usuario.Usuario;
